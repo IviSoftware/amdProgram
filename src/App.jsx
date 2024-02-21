ceb19bf4128f1a8c54f06158a0a7a1ff5d9851b0
iviSoftware <iva_men_ase@hotmail.com>
2024-02-21 16:50:44 -0600

    stable

--- src/App.jsx
+++ src/App.jsx
@@ -35,8 +35,8 @@ function App() {
         <div className='w-full flex flex-col items-center'>
         {section === 'curso' && <>
               
-              <section className='grid grid-cols-1 grid-rows-7 gap-5 md:w-3/5 '>
-                <div className='bg-custom-blue p-5'><h2 className='text-white text-center'>CENTRO DE CONVENCIONES Y AUDITORIO CINIA GONZÁLEZ DIEZ, HOSPITAL ESPAÑOL </h2></div>
+        <div className='bg-custom-blue p-5 w-full md:w-3/5 mb-10'><h2 className='text-white text-center'>CENTRO DE CONVENCIONES Y AUDITORIO CINIA GONZÁLEZ DIEZ, HOSPITAL ESPAÑOL </h2></div>
+              <section className='grid grid-cols-1 grid-rows-7 gap-4 md:w-3/5 '>
                 <RowTable hour="9:00 AM - 09:30 AM" doctor="Dra. Alejandra Rosales " theme="HISTORIA CLÍNICA" />
                 <RowTable hour="9:30 AM - 10:00 AM" doctor="Dra. Gabriela Ortega" theme="LESIONES ELEMENTALES" />
                 <RowTable hour="10:00 AM - 10:30 AM" doctor="Dra. Rebeca Rodríguez" theme="ACNÉ" />
@@ -45,8 +45,8 @@ function App() {
                 <RowTable hour="11:30AM - 12:00 AM" doctor="Dra. Gabriela Ortega" theme="FARMACODERMIAS" />
               </section>
               <div className='bg-custom-blue md:rounded-full
-              w-full md:w-3/5 flex flex-col items-center gap-2 md:grid md:grid-cols-3 md:justify-items-start md:p-5'><p className='text-white'>12:00 PM - 01:00 PM</p><h2 className='text-white text-center'>COMIDA</h2></div>
-              <section className='grid grid-cols-1 grid-rows-4 gap-12 md:w-3/5 mt-8'>
+              w-full md:w-3/5 flex flex-col items-center gap-4 mb-2 mt-2 md:grid md:grid-cols-3 md:justify-items-start md:p-5'><p className='text-white'>12:00 PM - 01:00 PM</p><h2 className='text-white text-center'>COMIDA</h2></div>
+              <section className='grid grid-cols-1 grid-rows-4 gap-4 md:w-3/5 mt-8'>
                   <RowTable hour="01:00 PM - 01:30 PM" doctor="Dra. Rebeca Rodríguez" theme="FOTOPROTECCIÓN" />
                   <RowTable hour="01:30PM - 02:00 PM" doctor="Dra. Susana Canalizo"  theme="CORTICOESTROPEO" />
                   <RowTable hour="02:00 PM - 02:30 PM" doctor="Dra. Alejandra Rosales" theme="URTICARIA" />
@@ -55,8 +55,8 @@ function App() {
         </> } 
 
         {section === 'pre-congreso' && <>
-            <section className='grid grid-cols-1 grid-rows-8 gap-5 md:w-3/5 '>
-                <div className='bg-custom-blue p-5'><h2 className='text-white text-center'>CENTRO DE CONVENCIONES Y AUDITORIO CINIA GONZÁLEZ DIEZ, HOSPITAL ESPAÑOL </h2></div>
+            <div className='bg-custom-blue p-5 w-full md:w-3/5 mb-10'><h2 className='text-white text-center'>CENTRO DE CONVENCIONES Y AUDITORIO CINIA GONZÁLEZ DIEZ, HOSPITAL ESPAÑOL </h2></div>
+            <section className='grid grid-cols-1 grid-rows-3 gap-4 md:w-3/5 '>
                 <RowTable hour="9:00 AM - 9:30 AM" doctor="Dr. Francisco Pérez Atamoroz" theme="TOXINA BOTULÍNICA" />
                 <RowTable hour="9:30 AM - 9:45 AM" doctor={["Preguntas y respuestas", <br />, "Dr. Francisco Pérez Atamoros"]}   />
                 <RowTable hour="9:45 AM - 10:15 AM" doctor="Dr. José Luis López Estebaranz" theme="RELLENOS" />
@@ -65,8 +65,10 @@ function App() {
                 <RowTable hour="11:15 AM - 11:30 AM" doctor={["Preguntas y respuestas",<br/>,"Dr. Diego del Ojo"]} />
                 <RowTable hour="11:30 PM - 12:00 PM" doctor="Mesa redonda"  />
             </section>
-            <div className='bg-custom-blue md:rounded-full p-2 w-full md:w-3/5 flex flex-col items-center gap-2 md:grid md:grid-cols-3 md:justify-items-start md:p-5'><p className='text-white'>12:00 PM - 01:00 PM</p><h2 className='text-white text-center'>COMIDA</h2></div>
-            <section className='grid grid-cols-1 grid-rows-3 gap-12 md:w-3/5 mt-8'>
+
+            <div className='bg-custom-blue md:rounded-full mb-6 mt-6 p-2 w-full md:w-3/5 flex flex-col items-center gap-2 md:grid md:grid-cols-3 md:justify-items-start md:p-5'><p className='text-white'>12:00 PM - 01:00 PM</p><h2 className='text-white text-center'>COMIDA</h2></div>
+            
+            <section className='grid grid-cols-1 grid-rows-3 gap-4 md:w-3/5 mt-8'>
                 <RowTable hour="01:00 PM - 2:00 PM" doctor="Dr. Francisco Pérez Atamoroz" theme="PRÁCTICA" />
                 <RowTable hour="2:00 PM - 03:00 PM" doctor="Dr. José Luis López Estebaranz"  theme="PRÁCTICA" />
                 <RowTable hour="03:00 PM - 04:00 PM" doctor="Dr. Francisco Pérez Atamoroz" theme="PRÁCTICA" />
--- src/assets/Components/RowTable.jsx
+++ src/assets/Components/RowTable.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 function RowTable({hour,doctor,theme}) {
   return (
-    <div className='w-full flex flex-col items-center gap-2 md:grid md:grid-cols-3 md:justify-items-start'>
+    <div className='w-full flex flex-col items-center pb-2 gap-2 md:grid md:grid-cols-3 md:justify-items-start border-b-2 border-blue-secondary'>
         <p>{hour}</p>
-        <p className='md:justify-self-center text-center'><b>{doctor}</b></p>
-        {theme && <p className='md:justify-self-end text-blue-secondary'>{theme}</p>}
+        <p><b>{doctor}</b></p>
+        {theme && <p className='text-blue-secondary'>{theme}</p>}
     </div>
   )
 }
